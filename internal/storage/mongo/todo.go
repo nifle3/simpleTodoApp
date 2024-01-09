09adaf757d03b19046a19e34f658a89751013c80
nifle3 <kuper116rus.2005@gmail.com>
2024-01-09 13:00:39 +0300

    add mongo storage todo method

--- internal/storage/mongo/todo.go
+++ internal/storage/mongo/todo.go
@@ -14,6 +14,8 @@ func (s Storage) AddTodo(userId string, todo domain.Todo, ctx context.Context) e
 		return err
 	}
 
+	todo.ID = primitive.NewObjectID().Hex()
+
 	push := bson.D{{Key: "$push", Value: bson.D{{Key: "todos", Value: todo}}}}
 
 	_, err = s.userCollection.UpdateByID(ctx, id, push)
@@ -21,14 +23,48 @@ func (s Storage) AddTodo(userId string, todo domain.Todo, ctx context.Context) e
 	return err
 }
 
-func (s Storage) DeleteTodo(todo domain.Todo, ctx context.Context) error {
-	return nil
+func (s Storage) DeleteTodo(userId string, todoId string, ctx context.Context) error {
+	id, err := primitive.ObjectIDFromHex(userId)
+	if err != nil {
+		return err
+	}
+
+	pull := bson.D{{Key: "$pull", Value: bson.D{{Key: "todos", Value: bson.D{{Key: "id", Value: todoId}}}}}}
+	_, err = s.userCollection.UpdateByID(ctx, id, pull)
+
+	return err
 }
 
-func (s Storage) UpdateTodo(todo domain.Todo, ctx context.Context) error {
-	return nil
+func (s Storage) UpdateTodo(userId string, todo domain.Todo, ctx context.Context) error {
+	id, err := primitive.ObjectIDFromHex(userId)
+	if err != nil {
+		return err
+	}
+
+	filter := bson.D{{Key: "_id", Value: id}, {Key: "todos.id", Value: todo.ID}}
+	updated := bson.D{{Key: "$set", Value: bson.M{
+		"todos.$.name":        todo.Name,
+		"todos.$.deadline":    todo.DeadLine,
+		"todos.$.description": todo.Description,
+		"todo.$.iscomplete":   todo.IsComplete,
+	}}}
+
+	_, err = s.userCollection.UpdateOne(ctx, filter, updated)
+
+	return err
 }
 
-func (s Storage) GetAllTodo(user domain.User, ctx context.Context) ([]domain.Todo, error) {
-	return nil, nil
+func (s Storage) GetAllTodo(userId string, ctx context.Context) ([]domain.Todo, error) {
+	id, err := primitive.ObjectIDFromHex(userId)
+	if err != nil {
+		return nil, err
+	}
+
+	var todos struct {
+		Todos []domain.Todo `bson:"todos"`
+	}
+
+	err = s.userCollection.FindOne(ctx, bson.D{{Key: "_id", Value: id}}).Decode(&todos)
+
+	return todos.Todos, err
 }
