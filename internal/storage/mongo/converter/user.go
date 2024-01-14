b04f6707004ddaad626ef5aa3125b8b10f5276ec
nifle3 <kuper116rus.2005@gmail.com>
2024-01-14 04:15:01 +0300

    add: user type converter in internal/storage/mongo/converter/user.go

---
+++ internal/storage/mongo/converter/user.go
@@ -0,0 +1,31 @@
+package converter
+
+import (
+	"todoApp/internal/domain"
+	"todoApp/internal/storage/mongo/object"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func ToMongo(user domain.User) (object.User, error) {
+	id, err := primitive.ObjectIDFromHex(user.ID)
+	if err != nil {
+		return object.User{}, err
+	}
+
+	return object.User{
+		ID:       id,
+		Login:    user.Login,
+		Password: user.Password,
+		Email:    user.Email,
+	}, nil
+}
+
+func ToDomain(user object.User) domain.User {
+	return domain.User{
+		ID:       user.ID.Hex(),
+		Login:    user.Login,
+		Password: user.Password,
+		Email:    user.Email,
+	}
+}
