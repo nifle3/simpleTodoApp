0e65eebf7d6c1d74db7521c297f56ba216384623
nifle3 <kuper116rus.2005@gmail.com>
2024-01-18 18:49:01 +0300

    add: get user in internal/http/user/get.go

--- internal/http/User/get.go
+++ internal/http/User/get.go
@@ -1,7 +1,24 @@
 package user
 
-import "net/http"
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+)
 
 func (r Router) Get(w http.ResponseWriter, rq *http.Request) {
 	w.Header().Add("Content-Type", "application/json")
+
+	result, err := r.useCase.Get("", context.Background())
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if err := json.NewEncoder(w).Encode(result); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
 }
--- internal/http/User/router.go
+++ internal/http/User/router.go
@@ -9,10 +9,10 @@ type UseCase interface {
 	Login(email, password string, ctx context.Context) (domain.User, error)
 	Add(user domain.User, ctx context.Context) error
 	Delete(userID string, ctx context.Context) error
-	UpdatePassword(userId, password string, ctx context.Context) error
-	UpdateEmail(userId, email string, ctx context.Context) error
-	UpdateLogin(userId, login string, ctx context.Context) error
-	Get()
+	UpdatePassword(userID, password string, ctx context.Context) error
+	UpdateEmail(userID, email string, ctx context.Context) error
+	UpdateLogin(userID, login string, ctx context.Context) error
+	Get(userID string, ctx context.Context) (domain.User, error)
 }
 
 type Router struct {
