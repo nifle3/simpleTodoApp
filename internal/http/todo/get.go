4352ffd597ec54d263ec325aeaaabb877df89f33
nifle3 <kuper116rus.2005@gmail.com>
2024-01-18 19:26:27 +0300

    add: getAll and getOne todo in internal/http/todo/get.go

---
+++ internal/http/todo/get.go
@@ -0,0 +1,52 @@
+package todo
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+)
+
+const (
+	todoQuery = "todo_id"
+)
+
+func (r Router) GetOne(w http.ResponseWriter, rq *http.Request) {
+	w.Header().Add("Content-type", "application/json")
+
+	if !rq.URL.Query().Has(todoQuery) {
+		http.Error(w, "have not todo_id parametr", http.StatusBadRequest)
+		return
+	}
+
+	todoID := rq.URL.Query().Get(todoQuery)
+
+	result, err := r.useCase.GetOne("", todoID, context.Background())
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if err := json.NewEncoder(w).Encode(result); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusInternalServerError)
+}
+
+func (r Router) GetAll(w http.ResponseWriter, rq *http.Request) {
+	w.Header().Add("Content-type", "application/json")
+
+	result, err := r.useCase.GetAll("", context.Background())
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if err := json.NewEncoder(w).Encode(result); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusInternalServerError)
+}
--- internal/http/todo/router.go
+++ internal/http/todo/router.go
@@ -6,9 +6,11 @@ import (
 )
 
 type UseCase interface {
-	AddTodo(userId string, todo domain.Todo, ctx context.Context) error
-	DeleteTodo(userId, todoId string, ctx context.Context) error
-	UpdateTodo(userId string, todo domain.Todo, ctx context.Context) error
+	Add(userID string, todo domain.Todo, ctx context.Context) error
+	Delete(userID, todoId string, ctx context.Context) error
+	Update(userID string, todo domain.Todo, ctx context.Context) error
+	GetOne(userID, todoID string, ctx context.Context) (domain.Todo, error)
+	GetAll(userID string, ctx context.Context) ([]domain.Todo, error)
 }
 
 type Router struct {
